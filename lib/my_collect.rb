3ea89d1687bf412effec19aa4799d3ce1005f2b0
Denise Henderson <denise@henderson3.us>
2020-07-12 11:21:58 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require binding.pry
 
+def my_collect(array)
+  i = 0
+  collection = []
+  while i < array.length
+    #binding.pry
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+#binding.pry
+hello(array) do |name|
+  name.split(" ").first
+end
